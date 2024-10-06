b8bc20fccab116a194b58c841d62c296ad124f88
Shay Bushary <shaybush93@gmail.com>
2024-10-06 11:27:22 +0300

    total_value function implemented

--- inventory_calculations.py
+++ inventory_calculations.py
@@ -2,3 +2,8 @@ class InventoryCalculations:
     def __init__(self, inventory):
         self.inventory = inventory
 
+    def total_value(self):
+        total = 0
+        for product in self.inventory.products:
+            total += product.price * product.quantity
+        return total
