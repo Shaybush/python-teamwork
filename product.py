ea3ac2808ab9a9bae4d0c77d2b7a82f1ae91e64f
natancohen <fl770fl@gmail.com>
2024-10-06 11:33:34 +0300

    class product

---
+++ product.py
@@ -0,0 +1,13 @@
+class product:
+    def __init__(self):
+        name = self.name
+        price = self.price
+        quantity = self. quantity
+
+    def update_quantity(self, amount):
+        self.quantity += amount
+
+    def get_info(self):
+        return f" name = {self.name}, price = {self.price}, quantity = {self.quantity}"
+
+
